909ba2b81c09f11e74dc39bac1b17c0d4f2a2b86
evan thompson <thompsonevanrobert@gmail.com>
2019-03-02 01:19:10 -0500

    when string delimited with commas is passed array of elements is returned

--- src/main/java/word/puzzle/kata/service/PuzzleParser.java
+++ src/main/java/word/puzzle/kata/service/PuzzleParser.java
@@ -14,12 +14,16 @@ public class PuzzleParser {
     List<String> wordsToFind = new ArrayList<>();
 
     public File readPuzzleFile() throws IOException {
-        File puzzle =  new File(puzzlePath+puzzleFileName);
+        File puzzle = new File(puzzlePath + puzzleFileName);
         BufferedReader puzzleReader = new BufferedReader(new FileReader(puzzle));
-        for(String wordToFind: puzzleReader.readLine().split(",")){
+        for (String wordToFind : puzzleReader.readLine().split(",")) {
             wordsToFind.add(wordToFind);
         }
         puzzleReader.close();
         return puzzle;
+    }
+
+    public String[] parseWordPuzzleLine(String testString) {
+        return testString.split(",");
     }
 }
--- src/test/java/word/puzzle/kata/service/PuzzleParserTest.java
+++ src/test/java/word/puzzle/kata/service/PuzzleParserTest.java
@@ -9,27 +9,35 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class PuzzleParserTest {
 
     PuzzleParser puzzleParser;
 
     @Before
-    public void setup(){
+    public void setup() {
         puzzleParser = new PuzzleParser();
-        ReflectionTestUtils.setField(puzzleParser,"puzzlePath",System.getProperty("user.dir") );
-        ReflectionTestUtils.setField(puzzleParser,"puzzleFileName","/puzzle.txt");
+        ReflectionTestUtils.setField(puzzleParser, "puzzlePath", System.getProperty("user.dir"));
+        ReflectionTestUtils.setField(puzzleParser, "puzzleFileName", "/puzzle.txt");
     }
 
     @Test
-    public void whenPuzzleFilePathIsPassedFileExistsInMemory() throws Exception{
+    public void whenPuzzleFilePathIsPassedFileExistsInMemory() throws Exception {
         assertThat(puzzleParser.readPuzzleFile().exists());
     }
 
     @Test
-    public void whenPuzzleIsReadWordsToFindAreAvailable()throws Exception{
+    public void whenPuzzleIsReadWordsToFindAreAvailable() throws Exception {
         puzzleParser.readPuzzleFile();
-        List<String> wordsToFind =(List)ReflectionTestUtils.getField(puzzleParser,"wordsToFind");
+        List<String> wordsToFind = (List) ReflectionTestUtils.getField(puzzleParser, "wordsToFind");
         assertThat(Arrays.asList(wordsToFind).contains("BONES"));
+    }
+
+    @Test
+    public void whenStringDelimitedWithCommaIsPassedArrayOfElementsIsReturned() throws Exception {
+        String testString = "H,E,L,O,W,R,L,D";
+        String[] testPuzzleLines = puzzleParser.parseWordPuzzleLine(testString);
+        assertEquals(8, testPuzzleLines.length);
     }
 }
