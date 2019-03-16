c3fd1e7e68482bb87084b62c33bc7249fbcf21d9
evan thompson <thompsonevanrobert@gmail.com>
2019-03-16 07:36:37 -0400

    Refactor test case to have a decouple test data setup helper class

--- src/test/java/word/puzzle/kata/service/PuzzleSolverTest.java
+++ src/test/java/word/puzzle/kata/service/PuzzleSolverTest.java
@@ -1,6 +1,7 @@
 package word.puzzle.kata.service;
 
 import org.junit.Test;
+import word.puzzle.kata.service.setup.DataSetup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -121,30 +122,8 @@ public class PuzzleSolverTest {
 
     @Test
     public void whenPuzzleIsPassedSampleFileSampleOutputIsPrinted(){
-        List<String> wordsToFind = new ArrayList<>();
-        wordsToFind.add("BONES");
-        wordsToFind.add("KHAN");
-        wordsToFind.add("KIRK");
-        wordsToFind.add("SCOTTY");
-        wordsToFind.add("SPOCK");
-        wordsToFind.add("SULU");
-        wordsToFind.add("UHURA");
-        PuzzleSolver solver = new PuzzleSolver(new String[][]{
-                {"U","M","K","H","U","L","K","I","N","V","J","O","C","W","E"},
-                {"L","L","S","H","K","Z","Z","W","Z","C","G","J","U","Y","G"},
-                {"H","S","U","P","J","P","R","J","D","H","S","B","X","T","G"},
-                {"B","R","J","S","O","E","Q","E","T","I","K","K","G","L","E"},
-                {"A","Y","O","A","G","C","I","R","D","Q","H","R","T","C","D"},
-                {"S","C","O","T","T","Y","K","Z","R","E","P","P","X","P","F"},
-                {"B","L","Q","S","L","N","E","E","E","V","U","L","F","M","Z"},
-                {"O","K","R","I","K","A","M","M","R","M","F","B","A","P","P"},
-                {"N","U","I","I","Y","H","Q","M","E","M","Q","R","Y","F","S"},
-                {"E","Y","Z","Y","G","K","Q","J","P","C","Q","W","Y","A","K"},
-                {"S","J","F","Z","M","Q","I","B","D","B","E","M","K","W","D"},
-                {"T","G","L","B","H","C","B","E","C","H","T","O","Y","I","K"},
-                {"O","J","Y","E","U","L","N","C","C","L","Y","B","Z","U","H"},
-                {"W","Z","M","I","S","U","K","U","R","B","I","D","U","X","S"},
-                {"K","Y","L","B","Q","Q","P","M","D","F","C","K","E","A","B"}}, wordsToFind);
+        DataSetup dataSetup = new DataSetup();
+        PuzzleSolver solver = dataSetup.setupRealPuzzle();
         assertEquals("BONES: (0,6),(0,7),(0,8),(0,9),(0,10)"+"\n"
                 +"KHAN: (5,9),(5,8),(5,7),(5,6)"+"\n"
                 +"KIRK: (4,7),(3,7),(2,7),(1,7)"+"\n"
